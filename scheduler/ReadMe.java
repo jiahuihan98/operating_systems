87944ada6331b1a494c17137b5ae6ceee0a70275
jiahuihan98 <46871764+jiahuihan98@users.noreply.github.com>
2019-11-19 16:15:44 -0500

    Add files via upload

---
+++ scheduler/ReadMe.java
@@ -0,0 +1,12 @@
+/*
+in teminal (command line)
+javac Scheduler.java
+java Scheduler --verbose filename.txt
+(OR withour verbose)
+java Scheduler filename.txt
+
+Also
+The input as 1 0 1 5 1 OR 1 (0 1 5 1) can both be read in correctly
+Otherwise the program may not run correctly.
+THANK YOU SOOOO MUCH!!!!! I know this is too much.
+*/
